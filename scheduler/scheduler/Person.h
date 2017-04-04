9da47422c3fed190217ccd05697819d7b3c58247
James Smith <computer.smither@gmail.com>
2017-04-03 21:04:26 -0700

    2017

---
+++ scheduler/scheduler/Person.h
@@ -0,0 +1,34 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+#include <vector>
+
+class Person {
+private:
+    std::string _name;
+    std::string _position;
+    float _pay;
+    float _max_hours;
+    float _hours_worked;
+    bool _is_working;
+    // std::vector<std::string> _days_off;
+public:
+    Person(std::string, std::string, float, float, float, bool);
+    //, std::string
+    void SetName(std::string);
+    void SetPosition(std::string);
+    void SetPay(float);
+    void SetMaxHours(float);
+    void SetHoursWorked(float);
+    void SetIsWorking(bool);
+    //void SetDaysOff(std::string);
+
+    std::string GetName();
+    std::string GetPosition();
+    float GetPay();
+    float GetMaxHours();
+    float GetHoursWorked();
+    bool GetIsWorking();
+    //std::string GetDaysOff(std::string);
+};
+#endif
